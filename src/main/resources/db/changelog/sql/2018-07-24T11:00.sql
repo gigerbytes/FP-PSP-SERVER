68667e650bc372dc20a786666adfbfa359ceca52
gabrielci <gabrielcarballude@gmail.com>
2018-07-26 08:32:24 -0400

    Update 2018-07-24T11:00.sql
    Fix

--- src/main/resources/db/changelog/sql/2018-07-24T11:00.sql
+++ src/main/resources/db/changelog/sql/2018-07-24T11:00.sql
@@ -1,4 +1,4 @@
 -- Existing NULL values for user_id were purged on Testing and 
 -- Demo (Production had none) 
 ALTER TABLE data_collect.snapshots_economics
-   ALTER COLUMN user_id DROP NOT NULL;
+   ALTER COLUMN user_id SET NOT NULL;
