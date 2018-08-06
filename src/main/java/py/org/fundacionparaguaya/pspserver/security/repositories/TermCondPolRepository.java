54f32bc42b8cd305d8adc0cba218bd99e769d772
Gabriel Carballude <gabrielcarballude@gmail.com>
2018-08-06 12:11:56 -0400

    Fix on TermCondPol available Languages endpoint - It was returning all available Terms and Priv Pol, now it only returns the latest pair per language as it should

--- src/main/java/py/org/fundacionparaguaya/pspserver/security/repositories/TermCondPolRepository.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/security/repositories/TermCondPolRepository.java
@@ -3,6 +3,8 @@ package py.org.fundacionparaguaya.pspserver.security.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import py.org.fundacionparaguaya.pspserver.security.constants.TermCondPolLocale;
 import py.org.fundacionparaguaya.pspserver.security.constants.TermCondPolType;
@@ -11,6 +13,9 @@ import py.org.fundacionparaguaya.pspserver.security.entities.TermCondPolEntity;
 public interface TermCondPolRepository extends
     JpaRepository<TermCondPolEntity, Long> {
     TermCondPolEntity findFirstByTypeCodAndApplicationIdAndLocaleOrderByIdDesc(
-            TermCondPolType type, Long applicationId, TermCondPolLocale locale);
-    List<TermCondPolEntity> findDistinctLocaleAndTypeCodByApplicationId(Long applicationId);
+            TermCondPolType type, Long applicationId, TermCondPolLocale locale);    
+
+    @Query(value = "SELECT DISTINCT ON (t.locale, t.type_cod) * FROM security.termcondpol t WHERE t.id_application = :appId ORDER BY t.locale, t.type_cod, t.created_date DESC",
+           nativeQuery = true)
+    List<TermCondPolEntity> findDistinctOnLocaleAndTypeCodByApplicationId(@Param("appId") Long applicationId);
 }
