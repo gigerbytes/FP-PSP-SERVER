3f44e71cc968b7f57a7f83fa47764d018125f816
Ale Feltes <afeltes@sodep.com.py>
2018-08-17 11:03:44 -0400

    Hotfix: Fixes a security warning message

--- src/main/java/py/org/fundacionparaguaya/pspserver/surveys/services/impl/SnapshotServiceImpl.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/surveys/services/impl/SnapshotServiceImpl.java
@@ -613,7 +613,7 @@ public class SnapshotServiceImpl implements SnapshotService {
     public Snapshot getSnapshotsById(Long snapshotId, UserDetailsDTO user) {
         Snapshot result = null;
         if (!user.hasRole(Role.ROLE_SURVEY_USER)) {
-            LOG.warn("[OPERATION_NOT_ALLOWED] Only {} can remove snapshots", Role.ROLE_SURVEY_USER);
+            LOG.warn("[OPERATION_NOT_ALLOWED] Only {} can get a snapshot", Role.ROLE_SURVEY_USER);
             return result;
         } else {
             SnapshotEconomicEntity economicEntity = economicRepository.findById(snapshotId);
